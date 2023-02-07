623bfa12018143a9ef500268c808e9c15c750e9b
TrickmanOff <egorkadv2002@gmail.com>
2023-02-07 18:40:15 +0300

    add histograms for wandb logger (to be changed)

--- wandb_logger.py
+++ wandb_logger.py
@@ -1,6 +1,6 @@
 import tempfile
 from copy import copy
-from typing import Dict, Any, Set, Optional, Iterable
+from typing import Dict, Any, Set, Optional, Iterable, Sequence
 
 import numpy as np
 import wandb
@@ -14,6 +14,7 @@ class _WandbLogger(GANLogger):
     """
     Should be used only from WandbCM
     """
+    WANDB_POINTS_LIMIT = 10_000
     PYPLOT_FORMAT = 'png'
 
     def __init__(self, config: Optional[LoggerConfig] = None):
@@ -28,31 +29,51 @@ class _WandbLogger(GANLogger):
         logged_dict[period] = period_index
         wandb.log(logged_dict)
 
-    def log_distribution(self, values: Dict[str, Iterable[float]], name: str,
-                         period: str, period_index: int) -> None:
-        keys = []
-        ys = []
+    def log_distribution(self, values: Dict[str, Sequence[float]], name: str,
+                         period: str, period_index: int,
+                         log_histogram: bool = True) -> None:
+        # limit
+        if log_histogram:
+            vals_per_key_cnt = int(self.WANDB_POINTS_LIMIT / len(values))
 
-        min_x = min(min(vals) for vals in values.values())
-        max_x = max(max(vals) for vals in values.values())
+            data = []
+            for key, vals in values.items():
+                if len(vals) > vals_per_key_cnt:
+                    vals = np.random.choice(vals, vals_per_key_cnt, replace=False)
+                data += [[key, val] for val in vals]
+            table = wandb.Table(data=data, columns=['type', 'value'])
+            fields = {
+                'groupKeys': 'type',
+                'value': 'value',
+                'title': name,
+            }
+            composite_histogram = wandb.plot_table(vega_spec_name="trickman/my_histogram",
+                                                   data_table=table, fields=fields)
+            wandb.log({name: composite_histogram})
+        else:
+            keys = []
+            ys = []
 
-        xs = np.linspace(min_x-0.1, max_x+0.1, num=25)
+            min_x = min(min(vals) for vals in values.values())
+            max_x = max(max(vals) for vals in values.values())
 
-        for key, vals in values.items():
-            keys.append(key)
-            kernel = gaussian_kde(vals)
-            y = kernel(xs)
-            ys.append(y)
+            xs = np.linspace(min_x-0.1, max_x+0.1, num=100)
 
-        wandb.log({name: wandb.plot.line_series(
-            xs=xs,
-            ys=ys,
-            keys=keys,
-            title=f'{name} {period}#{period_index}',
-            xname='value')})
+            for key, vals in values.items():
+                keys.append(key)
+                kernel = gaussian_kde(vals)
+                y = kernel(xs)
+                ys.append(y)
 
-    def log_critic_values_distribution(self, critic_values_true: Iterable[float],
-                                       critic_values_gen: Iterable[float],
+            wandb.log({name: wandb.plot.line_series(
+                xs=xs,
+                ys=ys,
+                keys=keys,
+                title=f'{name} {period}#{period_index}',
+                xname='value')})
+
+    def log_critic_values_distribution(self, critic_values_true: Sequence[float],
+                                       critic_values_gen: Sequence[float],
                                        period: str, period_index: int) -> None:
         self.log_distribution(
             values={
